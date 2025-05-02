c4d410cb0160faae8ebfca05e07758b5606b7ff4
Arin Kharkar <arinkharkar@gmail.cm>
2025-05-01 18:24:19 -0700

    Finished K16-Assembler v1.0

--- K16_Assembler/src/assembler.c
+++ K16_Assembler/src/assembler.c
@@ -29,6 +29,7 @@ int main(int argc, char** argv) {
 
     size_t output_file_size = 0;
     char* machine_code = assemble(input_file, &output_file_size);
+    free(input_file);
     if (!machine_code) {
         fprintf(stderr, "Error Assembling\n");
         return 1;
@@ -37,7 +38,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Error writing to %s\n", output_file_name);
         return 1;
     }
-
+    free(machine_code);
     return 0;
 }
 
@@ -97,40 +98,172 @@ int write_file(char* path, size_t file_len, char* write_contents) {
     return 0;
 }
 
-char* assemble(char* input_buf, size_t* output_file_size) {
-    // For now, create an abusurdly large buffer to write to
-    char* output_file_buf = malloc(32767);
+uint8_t* assemble(char* input_buf, size_t* output_file_size) { 
     size_t token_count = 0;
-    tokenize_input(input_buf, &token_count);
-
-
+    token* tokens = tokenize_input(input_buf, &token_count);
+    // used to free tokens
+    void* tokens_orig = tokens;
+    uint16_t* output_file_buf = malloc(sizeof(uint16_t)*(token_count+1));
+    if (!tokens) { exit(1); }
+    *output_file_size = token_count;
     // This assembler is very simple, go through each instruction and translate it directly to the OPCODE
-    return 0;
+    for (int i = 0; i < token_count; i++) {
+        switch (*tokens) {
+        case HLT:
+            output_file_buf[i] = (uint16_t)HLT;
+            break;
+        case LDA:
+            output_file_buf[i] = (uint16_t)LDA;
+            break;
+        case LDB:
+            output_file_buf[i] = (uint16_t)LDB;
+            break;
+        case LDC:
+            output_file_buf[i] = (uint16_t)LDC;
+            break;
+        case LDPC:
+            output_file_buf[i] = (uint16_t)LDPC;
+            break;
+        case LDPCNZ:
+            output_file_buf[i] = (uint16_t)LDPCNZ;
+            break;
+        case LDF:
+            output_file_buf[i] = (uint16_t)LDF;
+            break;
+        case PUSH:
+            output_file_buf[i] = (uint16_t)PUSH;
+        case PUSHI:
+            output_file_buf[i] = (uint16_t)PUSHI;
+            break;
+        case ADD:
+            output_file_buf[i] = (uint16_t)ADD;
+            break;
+        case LOADM:
+            output_file_buf[i] = (uint16_t)LOADM;
+            break;
+        case LOADVM:
+            output_file_buf[i] = (uint16_t)LOADVM;
+            break;
+        default:
+            fprintf(stderr, "Unknown Error, Unknown Token: %d\nHalting Assembler...\n", (uint16_t)*tokens);
+            break;
+        }
+        tokens++;
+    }
+    free(tokens_orig);
+    return (uint8_t*)output_file_buf;
 }
 
 // Essentially a lexer, turns the input file into a vector of tokens
 token* tokenize_input(char* input, size_t* token_amount) {
-    token* token_vec = malloc(sizeof(token)*50);
+    size_t max_tokens = 1;
+    token* token_vec = malloc(sizeof(token)*max_tokens);
+    if (!token_vec) {
+        fprintf(stderr, "Error initializing token_vec, insuficient heap\n");
+        return NULL;
+    }
     size_t token_amt = 0;
     char* cur = input;
-
-    char current_tok[16] = {0};
+    #define MAX_TOKEN_LENGTH 16
+    char current_tok[MAX_TOKEN_LENGTH] = {0};
     int i = 0;
-    while(*cur != '\0') {
-        if (*cur == ' ' || *cur == '\n') {
-            if (!strcmp(current_tok, "LDA")) {
+    while(true) {
+        if (*cur == ' ' || *cur == '\n' || *cur == '\0') {
+            // if we have reached 50 tokens, realloc tokens to twice the size it was previously
+            if (token_amt + 1 == max_tokens) {
+                max_tokens *= 2;
+                token_vec = realloc(token_vec, sizeof(token)*max_tokens);
+                if (!token_vec) {
+                    fprintf(stderr, "Error reinitializing token_vec, insuficient heap\n");
+                    return NULL;
+                }
+            }
+
+            if (!strcmp(current_tok, "HLT")) {
+                token_vec[token_amt] = HLT;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("HLT found\n");
+            } else if (!strcmp(current_tok, "LDA")) {
                 token_vec[token_amt] = LDA;
                 i = 0;
                 token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDA found\n");
             } else if (!strcmp(current_tok, "LDB")) {
                 token_vec[token_amt] = LDB;
                 i = 0;
                 token_amt++;
-            } else {
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDB found\n");
+            } else if (!strcmp(current_tok, "LDC")) {
+                token_vec[token_amt] = LDC;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDC found\n");
+            } else if (!strcmp(current_tok, "LDPC")) {
+                token_vec[token_amt] = LDPC;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDPC found\n");
+            } else if (!strcmp(current_tok, "LDPCNZ")) {
+                token_vec[token_amt] = LDPCNZ;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDPCNZ found\n");
+            } else if (!strcmp(current_tok, "LDF")) {
+                token_vec[token_amt] = LDF;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LDF found\n");
+            } else if (!strcmp(current_tok, "PUSH")) {
+                token_vec[token_amt] = PUSH;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("PUSH found\n");
+            } else if (!strcmp(current_tok, "ADD")) {
+                token_vec[token_amt] = ADD;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("ADD found\n");
+            } else if (!strcmp(current_tok, "LOADM")) {
+                token_vec[token_amt] = LOADM;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LOADM found\n");
+            } else if (!strcmp(current_tok, "LOADVM")) {
+                token_vec[token_amt] = LOADVM;
+                i = 0;
+                token_amt++;
+                // overwrite the token to 0
+                memset(current_tok, 0, MAX_TOKEN_LENGTH);
+                printf("LOADVM found\n");
+            // If the current token isnt empty (which could happen if the last line had an extra \n char), then we have an undefined token
+            } else if(strlen(current_tok) != 0) {
                 fprintf(stderr, "ERROR: Unrecognized Token: %s\n", current_tok);
-                printf("len: %ld\n", strlen(current_tok));
                 exit(1);
             }
+
+            // if we reached the end of the file
+            if (*cur == '\0') break;
         } else {
             current_tok[i] = *cur;
             i++;
@@ -138,4 +271,5 @@ token* tokenize_input(char* input, size_t* token_amount) {
         cur++;  
     }
     *token_amount = token_amt;
+    return token_vec;
 }
